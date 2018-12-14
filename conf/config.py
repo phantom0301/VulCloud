ff61087b9a91dd0164798a72ba2b5e7e6e03c114
test_helper <phantom0309@sina.com>
2018-12-14 21:43:20 +0800

    update

--- conf/config.py
+++ conf/config.py
@@ -4,9 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '0adc486137a8b244f819392aac02e8c'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     DB_HOST = '127.0.0.1'
-    DB_USER = 'VULCloud'
+    DB_USER = 'root'
     DB_PASSWD = '123456'
     DB_DATABASE = 'VULCloud'
     ITEMS_PER_PAGE = 10
